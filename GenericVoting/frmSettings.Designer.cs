30a935567243f659545064d510d0cfb9404893e9
Glenn <Glenn@LARS>
2017-03-12 20:18:17 +0800

    settingsForm2.0

--- GenericVoting/frmSettings.Designer.cs
+++ GenericVoting/frmSettings.Designer.cs
@@ -30,37 +30,68 @@
         {
             this.btnReset = new System.Windows.Forms.Button();
             this.btnRules = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(88, 30);
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(71)))));
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnReset.Location = new System.Drawing.Point(48, 116);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(187, 65);
+            this.btnReset.Size = new System.Drawing.Size(147, 58);
             this.btnReset.TabIndex = 0;
             this.btnReset.Text = "RESET";
-            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // btnRules
             // 
-            this.btnRules.Location = new System.Drawing.Point(88, 139);
+            this.btnRules.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(71)))));
+            this.btnRules.FlatAppearance.BorderSize = 0;
+            this.btnRules.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRules.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRules.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnRules.Location = new System.Drawing.Point(48, 202);
+            this.btnRules.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRules.Name = "btnRules";
-            this.btnRules.Size = new System.Drawing.Size(187, 78);
+            this.btnRules.Size = new System.Drawing.Size(147, 58);
             this.btnRules.TabIndex = 1;
             this.btnRules.Text = "RULES";
-            this.btnRules.UseVisualStyleBackColor = true;
+            this.btnRules.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(79, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 26);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Settings";
             // 
             // frmSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(431, 350);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(242, 271);
+            this.ControlBox = false;
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnRules);
             this.Controls.Add(this.btnReset);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmSettings";
-            this.Text = "frmSettings";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +99,6 @@
 
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Label label6;
     }
 }
